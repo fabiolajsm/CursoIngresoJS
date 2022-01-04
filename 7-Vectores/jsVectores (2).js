bab75740b7a270189b76449fe8561eb377fd0cad
Fabiola <you@example.com>
2022-01-04 11:51:47 -0300

    ejercicio 2 vectores

--- 7-Vectores/jsVectores (2).js
+++ 7-Vectores/jsVectores (2).js
@@ -1,25 +1,15 @@
-function mostrar()
-{
+function mostrar() {
+  var miArray = new Array()
 
-	var miArray = new Array();
+  miArray[0] = 'Juan'
+  miArray[1] = 'Luis'
+  miArray[2] = 'María'
+  miArray[3] = 'Nahuel'
+  miArray[4] = 'Yolanda'
 
-	miArray[0]="Juan";
-	miArray[1]="Luis";
-	miArray[2]="Maria";
-	miArray[3]="Nahuel";
-	miArray[4]="Yolanda";
+  document.write('El contenido del vector es: <br>')
 
-
-document.write("El contenido del vector es: <br>");
-
-	for(var i=0; i<5; i++)
-	{
-
-		document.write("Posicion "+i+"-->"+miArray[i]+"<br>");
-	}
-
-
-
-
-
-}//FIN DE LA FUNCIÓN
+  for (var i = 0; i < 5; i++) {
+    document.write('Posicion ' + i + '--> ' + miArray[i] + '<br>')
+  }
+} //FIN DE LA FUNCIÓN
