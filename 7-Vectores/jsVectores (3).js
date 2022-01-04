2954268a923349583e5a0f2a286928764966ccff
Fabiola <you@example.com>
2022-01-04 11:55:07 -0300

    ejercicio 3 vectores

--- 7-Vectores/jsVectores (3).js
+++ 7-Vectores/jsVectores (3).js
@@ -1,23 +1,11 @@
-function mostrar()
-{
+function mostrar() {
+  var miArray = new Array()
 
-	var miArray = new Array();
+  for (var i = 0; i < 5; i++) {
+    miArray[i] = parseInt(prompt('Ingrese un número'))
+  }
 
-	for (var i = 0; i <5; i++)
-	{
-		miArray[i]=parseInt(prompt("Ingrese un numero"));
-	}
-
-
-
-	for(var i=0; i<5; i++)
-	{
-
-		document.write("Posicion "+i+"-->"+miArray[i]+"<br>");
-	}
-
-
-
-
-
-}//FIN DE LA FUNCIÓN
+  for (var i = 0; i < 5; i++) {
+    document.write('Posicion ' + i + '--> ' + miArray[i] + '<br>')
+  }
+} //FIN DE LA FUNCIÓN
