a50ced7337fe4ce23bcec55e8f9131ba74081273
Fabiola <you@example.com>
2022-01-06 10:02:05 -0300

    Tps ejercicio 1

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -3,15 +3,26 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
-{
-	
+
+function Sumar(total) {
+  const p1 = parseInt(txtIdPrecioUno.value)
+  const p2 = parseInt(txtIdPrecioDos.value)
+  const p3 = parseInt(txtIdPrecioTres.value)
+  const sum = p1 + p2 + p3
+
+  if (total !== true) {
+    alert(sum)
+  } else {
+    return sum
+  }
 }
-function Promedio () 
-{
-	
+
+function Promedio() {
+  let suma = Sumar(true)
+  alert(suma / 3)
 }
-function PrecioFinal () 
-{
-	
+
+function PrecioFinal() {
+  let suma = Sumar(true)
+  alert(suma + suma * 0.21)
 }
