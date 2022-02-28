d409b0ce42aa46726b6b9b19e8c8f437ae34369c
Fabiola Suarez <fsuarez@technisys.com>
2022-02-28 14:40:37 -0300

    Ejercicio 4 parcial 2019

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,24 @@
-function mostrar()
-{
+function mostrar() {
+  let numeroUno;
+  let numeroDos;
+  let resta;
 
+  numeroUno = prompt("Ingrese un numero");
+  numeroDos = prompt("Ingrese un numero");
+
+  numeroUno = parseInt(numeroUno);
+  numeroDos = parseInt(numeroDos);
+
+  if (numeroUno === numeroDos) {
+    alert(numeroUno + ", " + numeroDos);
+  }
+  if (numeroUno > numeroDos) {
+    resta = numeroUno - numeroDos;
+    alert(`La resta de ${numeroUno} con ${numeroDos} es igual a : ${resta}`);
+    if (resta > 10) {
+      alert(`La resta es ${resta} y superó el 10`);
+    }
+  } else {
+    alert(`ok!`);
+  }
 }
