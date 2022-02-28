332b2f1bb659c0750bcf9385c6a932de86a5d67a
Fabiola Suarez <fsuarez@technisys.com>
2022-02-28 14:10:19 -0300

    Ejercicio 2 parcial 2019

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,24 @@
-function mostrar()
-{
-  
+function mostrar() {
+  let nombreUno;
+  let nombreDos;
+  let pesoUno;
+  let pesoDos;
+  let sumaPesos;
+  let promedioPeso;
+  let mensaje;
+
+  nombreUno = prompt("Ingrese su nombre:");
+  nombreDos = prompt("Ingrese el nombre de su pareja:");
+
+  pesoUno = prompt("Ingrese su peso:");
+  pesoDos = prompt("Ingrese el peso de su pareja:");
+
+  pesoUno = parseInt(pesoUno);
+  pesoDos = parseInt(pesoDos);
+
+  sumaPesos = pesoUno + pesoDos;
+  promedioPeso = sumaPesos / 2;
+
+  mensaje = `Ustedes se llaman ${nombreUno} y ${nombreDos}. Pesan ${pesoUno} y ${pesoDos} kilos, que sumados son ${sumaPesos} kilos y el promedio de peso ${promedioPeso} ".`;
+  alert(mensaje);
 }
