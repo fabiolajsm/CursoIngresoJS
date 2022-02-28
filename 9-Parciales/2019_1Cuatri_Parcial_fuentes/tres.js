e8e82ef27e7b51d9e7b62a7a6137113a49231acc
Fabiola Suarez <fsuarez@technisys.com>
2022-02-28 14:25:01 -0300

    Ejercicio 3 parcial 2019

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,22 @@
-function mostrar()
-{
+function mostrar() {
+  let precio;
+  let porcentajeDescuento;
+  let precioFinal;
 
+  precio = prompt("Ingrese el precio:");
+  precio = parseInt(precio);
+
+  porcentajeDescuento = prompt(
+    "Ingrese el porcentaje de descuento(en numeros enteros):"
+  );
+  while (porcentajeDescuento < 0) {
+    porcentajeDescuento = prompt(
+      "Ingrese el porcentaje de descuento(en numeros enteros):"
+    );
+  }
+  porcentajeDescuento = parseInt(porcentajeDescuento);
+  porcentajeDescuento = (precio * porcentajeDescuento) / 100;
+
+  precioFinal = precio - porcentajeDescuento;
+  elPrecioFinal.value = precioFinal;
 }
